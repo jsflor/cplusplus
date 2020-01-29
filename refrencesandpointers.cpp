09d048efc4673478125948b3b98440d02de2de71
Juan Sebastian Flor Usma <50371169+sebastianflor@users.noreply.github.com>
2020-01-29 12:44:40 +0100

    Add files via upload

---
+++ refrencesandpointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    /*References*/
+    string name;
+    string &nameref = name; //Reference to name
+
+    name = "Sebastian";
+    cout << name << "\n";   //output value of name (Sebastian)
+    cout << nameref << "\n";    //output reference to name (Sebastian)
+
+    /*Memory Address*/
+    cout << &name << "\n"; //output memory address of name (xxxxxx)
+
+    /*Pointers*/
+    string* nameptr = &name;
+
+    cout << nameptr << "\n"; // Reference: Output the memory address of name with the pointer (xxxxxx)
+    cout << *nameptr << "\n"; // Dereference: Output the value of name with the pointer (Sebastian)
+
+    return 0;
+}
